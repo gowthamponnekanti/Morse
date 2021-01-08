eadd5ed0f3f60737aaa3e0b40f229addd572fde8
Gowtham Sai <46836082+gowthamponnekanti@users.noreply.github.com>
2021-01-08 21:54:06 +0530

    Delete audi.py

--- audi.py
+++
@@ -1,9 +0,0 @@
-import speech_recognition as sr
-print('speak now !!!')
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    audio = r.listen(source)
-try:
-    print(r.recognize_google(audio))
-except Exception:
-    print("Something went wrong !!")
